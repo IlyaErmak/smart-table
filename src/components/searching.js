9c8b4245d9b3c54bd3d8078bc0ba0d8a539354c4
IlyaErmak <ermack.58@yandex.ru>
2025-08-11 14:08:03 +0300

    Add files via upload

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,12 +1,4 @@
-import {rules, createComparison} from "../lib/compare.js";
-
-
 export function initSearching(searchElement, searchField) {
-    const compare = createComparison([
-        rules.skipEmptyTargetValues,
-        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
-    ]);
-
     return (query, state, action) => { // result заменили на query
         return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
             search: state[searchField] // устанавливаем в query параметр
